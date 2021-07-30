1cb58cbfae3c2ccbc5d0d708b115fd0d62371321
Amri Hakim Sihite, ST <73640086+amrihakims@users.noreply.github.com>
2021-07-30 13:06:22 +0700

    Update script.js
    sedikit update-an

--- coding/script.js
+++ coding/script.js
@@ -17,12 +17,14 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbwOi7chKubwmJwydH1SZ4
 const form = document.forms['formDaftar'];
 const biaya = document.querySelector('#total');
 const kirim = document.querySelector('#kirim');
+const clear = document.querySelector('#bersihkan');
 const loading = document.querySelector('#load');
 const alertUser = document.querySelector('#alertuser');
 
 form.addEventListener('submit', e => {
 	e.preventDefault();
 	kirim.style.display='none';
+	loading.style.display='inline';
 	biaya.style.display='none';
 	fetch(scriptURL, { 
 	 	method: 'POST', body: new FormData(form)
@@ -33,9 +35,16 @@ form.addEventListener('submit', e => {
 	 	alertUser.style.backgroundColor='lightblue';
 	 	alertUser.style.color='darkgreen';
 	 	loading.style.display='none';
+	 	clear.style.display='inline';
 	 	})
 
 	.catch(error => console.error('Error!', error.message))
+})
+
+form.addEventListener('reset', function() {
+	clear.style.display='none';
+	alertUser.style.display='none';
+	IsiNama='setFocus';
 })
 
 function ShowBar() {
